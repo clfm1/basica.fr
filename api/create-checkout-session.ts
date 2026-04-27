a1830fe3916765cdc8b91c4a6fffcc3a0e8a4ad4
clfm1 <clfm1@users.noreply.github.com>
2026-04-27 23:41:41 +0200

    fix: hardcode API to CommonJS to bypass Vercel ESM syntax errors completely

--- api/create-checkout-session.ts
+++ api/create-checkout-session.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { getStripe, setCors } from "./_utils.js";
+import { getStripe, setCors } from "./_utils";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   setCors(res);
